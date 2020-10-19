66cc16fa8cc5f3315aa536719127f6d104320521
lidianxiang <ldx2703@gmail.com>
2020-10-19 09:47:15 +0800

    两数之和

---
+++ 数组/1-两数之和.cpp
@@ -0,0 +1,59 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+#include<unordered_map>
+
+/*
+
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+*/
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+class Solution2
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end())
+            {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
