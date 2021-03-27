e3e9d85228236e0e842b83d868354ddc14f986ac
lidianxiang <ldx2703@gmail.com>
2021-03-27 08:53:33 +0800

    区域和检索-数组不可变

---
+++ 数组/303-区域和检索-数组不可变.cpp
@@ -0,0 +1,47 @@
+/*
+
+给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ 
+
+示例：
+
+输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+
+*/
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+
+class NumArray{    // 前缀和
+private:
+    vector<int> sum;
+public:
+    NumArray(vector<int>& nums){
+        int n = nums.size();
+        sum.resize(n+1);
+        for(int i=0; i<n; i++){
+            sum[i+1] = sum[i] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right){
+        return sum[right+1] - sum[left];
+    }
+};
