efe259262dd42116247208817c73800478cded57
lidianxiang <ldx2703@gmail.com>
2021-04-14 09:43:25 +0800

    爬楼梯

---
+++ 动态规划/70-爬楼梯.cpp
@@ -0,0 +1,63 @@
+/*
+
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+示例 1：
+
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+示例 2：
+
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+
+*/
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+
+class Solution{   // 动态规划
+public:
+    int climbStairs(int n){
+        if (n <= 1)
+            return n;
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+
+class Solution2{   // 动态规划 + 空间优化
+public:
+    int climbStairs(int n){
+        if (n <= 1)
+            return n;
+        int dp[3];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            int sum = dp[1] + dp[2];
+            dp[1] = dp[2];
+            dp[2] = sum;
+        }
+        return dp2;
+    }
+};
